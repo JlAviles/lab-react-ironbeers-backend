2d50d422a16c32a4a99e6ef28a2d6b810de29d70
Dani Vicario <danigv.email@gmail.com>
2019-05-02 13:06:14 +0200

    added endpoints

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router  = express.Router();
+const Task = require("../models/Task")
+
+router.get('/tasks', (req, res, next) => {
+  Task
+    .find()
+    .select({
+      "__v": false,
+      "updatedAt": false
+    })
+    .then(allTheTasks => res.json(allTheTasks))
+});
+
+
+router.post('/task', (req, res, next) => {
+  Task.create({
+    "taskStr" : req.body.taskStr,
+    "done" : false
+  }).then((savedTask) => res.json(savedTask))
+});
+
+module.exports = router;
